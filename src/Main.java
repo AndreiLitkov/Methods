c2bb5e03495a353e3061709fbbb39801e09468c4
Andrei <litkov99@mail.ru>
2025-06-12 13:35:08 +0300

    Completed HW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void isYearLeap( int year){
+        if ((year%4==0 && year%100!=0) || (year%400==0)) {
+            System.out.println(year+ " год  - високосный год");
+        }else if(year<1584){
+            System.out.println(year+ " год. В этом году еще не было выбрано такое правило про определения вискокосного года.");
+        }else{
+            System.out.println(year+ " год - не високосный год");
         }
+    }
+    public static void whatProgramInstall( int clientOs , int clientDeviceYear){
+        if (clientOs==0){
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        }else if(clientDeviceYear<2015){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if(clientOs==1){
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }else if(clientDeviceYear<2015){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }
+    }
+    public static void deliveryDays(int distance){
+        if (distance<20){
+            System.out.println("Потребуется 1 день для доставки");
+        }else if (distance>=20 && distance<=60){
+            System.out.println("Потребуется 2 дня для доставки");
+        }else if(distance>60 && distance<=100){
+            System.out.println("Потребуется 3 дня для доставки");
+        }else{
+            System.out.println("Доставки нет");
+        }
+    }
+    public static void main(String[] args) {
+
+        //Task 1
+        System.out.println("Task 1");
+        int year = 1800;
+        isYearLeap(year);
+
+        //Task 2
+        System.out.println("Task 2");
+        int clientOSNew = 0;
+        int clientDeviceYear = 2013;
+        whatProgramInstall(clientOSNew , clientDeviceYear);
+
+        //Task 3
+        System.out.println("Task 3");
+        int deliveryDistance = 95;
+        deliveryDays(deliveryDistance);
     }
 }
